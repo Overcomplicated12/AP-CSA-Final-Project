e16c61f71ecd5a5ed39faedd23323345de9c99cd
Frank <frankwangli0326@gmail.com>
2026-06-01 16:12:09 -0400

    create main class
    Signed-off-by: Frank <frankwangli0326@gmail.com>

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Pin-Ball");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // TODO: Create the panel here
+        frame.setSize(600, 650);
+        frame.setVisible(true);
+    }
+}

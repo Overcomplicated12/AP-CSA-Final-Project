a86bbd36f434f2a343e1c5224a9468f958a0771a
overcomplicated12 <frankwangli0326@gmail.com>
2026-06-12 07:35:47 -0400

    gameobject rework
    Signed-off-by: overcomplicated12 <frankwangli0326@gmail.com>

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,62 +1,57 @@
 import java.awt.Rectangle;
 import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.Shape;
 
-public class GameObject
-{
-    private int x,y,width,height;
+public class GameObject {
+    private int x, y, width, height;
     private Color color;
-    public GameObject(int x, int y, int width, int height, Color color)
-    {
-        this.x=x;
-        this.y=y;
-        this.width=width;
-        this.height=height;
-        this.color=color;
+
+    public GameObject(int x, int y, int width, int height, Color color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
     }
 
-    public void draw(Graphics g)
-    {
+    public void draw(Graphics g) {
 
     }
 
-    public Rectangle getBounds()
-    {
-        return new Rectangle(x,y,width,height);
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, width, height);
     }
 
-    public int getX()
-    {
+    public Shape getShape() {
+        return getBounds();
+    }
+
+    public int getX() {
         return x;
     }
 
-    public int getY()
-    {
+    public int getY() {
         return y;
     }
 
-    public int getWidth()
-    {
+    public int getWidth() {
         return width;
     }
 
-    public int getHeight()
-    {
+    public int getHeight() {
         return height;
     }
 
-    public Color getColor()
-    {
+    public Color getColor() {
         return color;
     }
 
-    public void setX(int x)
-    {
-        this.x=x;
+    public void setX(int x) {
+        this.x = x;
     }
 
-    public void setY(int y)
-    {
-        this.y=y;
+    public void setY(int y) {
+        this.y = y;
     }
 }
